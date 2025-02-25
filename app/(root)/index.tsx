30a80844d3e7cb9dea1964bea7a7cde23683d6f0
Your Name <you@example.com>
2025-02-25 13:42:03 +0700

    feature/110

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-    return <Redirect href="/(root)/(auth)/sign-up" />;
+    return <Redirect href="/(root)/(auth)/sign-in" />;
 }
