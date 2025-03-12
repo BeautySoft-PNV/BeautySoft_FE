427db53fb223e9de8f5c38c2390a0ff0e94bcab1
Huỳnh Thị Tố Loan <loan.huynh25@student.passerellesnumeriques.org>
2025-03-12 10:01:26 +0700

    Fix conflic in Generate.tsx

--- app/(root)/generate.tsx
+++ app/(root)/generate.tsx
@@ -14,7 +14,7 @@ import {
 } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import Avatar from "@/components/avatar";
-import * as FileSystem from "expo-file-system";s
+import * as FileSystem from "expo-file-system"; // For native platforms
 import { Asset } from "expo-asset";
 import { AntDesign } from "@expo/vector-icons";
 import { router } from "expo-router";
